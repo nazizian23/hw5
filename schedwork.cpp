5a3173d769a3b5be36cf90b2870d021af6938b22
Nick Azizian <nazizian@usc.edu>
2025-04-23 23:11:12 +0000

    Final submission hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -22,6 +22,16 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
+bool assignWorker(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<int>& shiftsWorked,
+    size_t day,
+    size_t slot
+);
+
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +47,54 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    size_t numDays= avail.size();
+    size_t numWorkers=avail[0].size();
+    //Initialize schedule matrix
+    for(size_t i=0; i<numDays;++i){
+        sched.push_back(vector<Worker_T>());
+    }
+    //Initialize shift count for each worker
+    vector<int>shiftsWorked(numWorkers,0);
+    //Start recursion
+    return assignWorker(avail,dailyNeed,maxShifts,sched,shiftsWorked,0,0);
 
+}
 
-
-
+bool assignWorker(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<int>& shiftsWorked,
+    size_t day,
+    size_t slot
+)
+{
+    size_t numDays= avail.size();
+    size_t numWorkers=avail[0].size();
+    //Base case for scenario where all days are scheduled
+    if(day==numDays){
+        return true;
+    }
+    //If the current day is full move on to next day 
+    if(slot==dailyNeed){
+        return assignWorker(avail,dailyNeed,maxShifts,sched,shiftsWorked,day+1,0);
+    }
+    //Try all the workers 
+    for(size_t worker=0; worker<numWorkers;++worker){
+        if(avail[day][worker]&&shiftsWorked[worker]<static_cast<int>(maxShifts)&& find(sched[day].begin(),sched[day].end(),worker)==sched[day].end()){
+            //If all constraints pass assign the worker to the current slot
+            sched[day].push_back(worker);
+            shiftsWorked[worker]++;//Update their shift count
+            //Recurse to fill the next slot 
+            if(assignWorker(avail,dailyNeed,maxShifts,sched,shiftsWorked,day,slot+1)){
+                return true;
+            }
+            //Backtrack if recursion fails 
+            sched[day].pop_back();
+            shiftsWorked[worker]--;
+        }
+    }
+    return false;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -14,7 +14,9 @@ using namespace std;
 
 // Add prototypes of helper functions here
 
+bool containsAllFloating(const string& word, const string& floating);
 
+void recurseWordle(string& curr, int index,string floating, const set<string>& dict, set<string>& results);
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
@@ -22,7 +24,103 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<string> results;
+    string curr=in;
+    string adjustedFloating=floating;
+    //removes the fixed position chars from the floating list
+    for(size_t i=0;i<curr.size();++i){
+        char c=curr[i];
+        size_t pos=adjustedFloating.find(c);
+        if(c!='-'&& pos!=string::npos){
+            adjustedFloating.erase(pos,1);//removes the first occurrence
+        }
+    }
+    // begin recursion
+    recurseWordle(curr,0,adjustedFloating,dict,results);
+    
+    return results;
 
 }
 
 // Define any helper functions here
+
+//Checks if all floating letters exist in the final word
+bool containsAllFloating(const string& word, const string& floating){
+    //make a temp copy of word so we can find used letters and mark them
+    string temp=word;
+
+    //For each floating letter check if its in the temp word
+    for(size_t i=0;i<floating.size();++i){
+        char c=floating[i];
+        //find first occurrence of the char
+        size_t pos=temp.find(c);
+        //if its not found the word is missing a required floating letter
+        if(pos==string::npos){
+            return false;
+        }
+        temp[pos]='*';
+    }
+
+    return true;
+
+
+
+
+}
+
+
+//Recursive helper to generate words char by char
+void recurseWordle(string& curr, int index,string floating, const set<string>& dict, set<string>& results){
+
+    //Base case when the entire word is formed
+    if(index==curr.size()){
+        //Check if all the letters are used and word is correct
+        if(containsAllFloating(curr,floating) && dict.find(curr)!=dict.end()){
+            results.insert(curr);
+        }
+        return;
+    }
+    //Counts the remaining blank spaces from the current index on
+    int blanksLeft=0;
+    for(int i=index;i<curr.size();++i){
+        if(curr[i]=='-'){
+            ++blanksLeft;
+        }
+    }
+    //If not enough blanks left to place the letters stop
+    if(blanksLeft<floating.size()){
+        return;
+    }
+    //If the current char is already fixed continue to next index
+    if(curr[index]!='-'){
+        recurseWordle(curr,index+1,floating,dict,results);
+    }
+    else{
+        //Try lowercase letters
+        for(char c= 'a';c<='z';++c){
+            curr[index]=c;
+            //copy the floating list to update if we use the letter
+            size_t pos=floating.find(c);
+            string nextFloating=floating;
+            //if the letter is in the floating list remove one instance of it
+            if(pos!=string::npos){
+                nextFloating.erase(pos,1);
+                recurseWordle(curr,index+1,nextFloating,dict,results);
+            }
+            //if not in floating, only continue if there are enough blanks left
+            else if(floating.size()<blanksLeft){
+                recurseWordle(curr,index+1,floating,dict,results);
+
+            }
+            
+        }
+
+        curr[index]='-';
+    }
+
+
+}
+
+
+
+
